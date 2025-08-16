67a6bb990bf642e8a84416c14e92d1a3836a40a9
Ariel Shnits <ariel@Ariels-Laptop.local>
2025-08-16 18:20:39 +0300

    create manager logic that can spawn docker containers by image name

--- src/manager/__main__.py
+++ src/manager/__main__.py
@@ -1,2 +1,5 @@
+from ..manager import docker_utils
+
 if __name__ == "__main__":
-    print("hello bootstrap!")
+    print("hello manager!")
+    docker_utils.spawn_and_wait_for_container("vault-bootstrap")
