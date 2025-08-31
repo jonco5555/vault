78d949e937b59af105e0a43734707b6bf629885f
Yonatan Cohen <jonco5555@gmail.com>
2025-08-31 23:44:41 +0300

    added tests to bootstrap server using grpcio-testing package, fixed share_server to work with encrypted shares

--- src/vault/share_server/share_server.py
+++ src/vault/share_server/share_server.py
@@ -1,28 +1,34 @@
 import grpc
 
-from vault.common.generated.vault_pb2 import DecryptResponse, Key, StoreShareResponse
+from vault.common.generated.vault_pb2 import DecryptResponse, StoreShareResponse
 from vault.common.generated.vault_pb2_grpc import ShareServerServicer
+from vault.common.types import Key
+from vault.crypto.asymmetric import decrypt, generate_key_pair
 from vault.crypto.threshold import partial_decrypt
 
 
 class ShareServer(ShareServerServicer):
     def __init__(self):
-        self._shares: dict[Key] = {}
+        self._privkey_b64, self._pubkey_b64 = generate_key_pair()
+        self._encrypted_shares: dict[bytes] = {}
+
+    # TODO: Register to manager as server
 
     async def StoreShare(self, request, context):
-        if request.user_id in self._shares:
+        if request.user_id in self._encrypted_shares:
             context.set_code(grpc.StatusCode.ALREADY_EXISTS)
             context.set_details("Share for this user already exists.")
             return StoreShareResponse(success=False)
-        self._shares[request.user_id] = request.share
+        self._encrypted_shares[request.user_id] = request.encrypted_share
         return StoreShareResponse(success=True)
 
     async def Decrypt(self, request, context):
-        if request.user_id not in self._shares:
+        if request.user_id not in self._encrypted_shares:
             context.set_code(grpc.StatusCode.NOT_FOUND)
             context.set_details("No share found for this user.")
             return DecryptResponse()
-        partial_decrypted = partial_decrypt(
-            request.secret, self._shares.get(request.user_id)
+        share = Key.model_validate_json(
+            decrypt(self._encrypted_shares.get(request.user_id), self._privkey_b64)
         )
+        partial_decrypted = partial_decrypt(request.secret, share)
         return DecryptResponse(partial_decrypted=partial_decrypted)
---
+++ tests/test_bootstrap.py
@@ -0,0 +1,114 @@
+import grpc
+import grpc_testing
+import pytest
+from grpc_testing._server._server import _Server
+
+from vault.bootstrap.bootstrap import Bootstrap
+from vault.common.generated.vault_pb2 import DESCRIPTOR, GenerateSharesRequest
+from vault.common.types import Key
+from vault.crypto.asymmetric import decrypt, generate_key_pair
+
+
+@pytest.fixture
+def key_pairs(request) -> tuple[list[bytes], list[bytes]]:
+    n = request.param
+    privates = []
+    publics = []
+    for i in range(n):
+        priv, pub = generate_key_pair()
+        privates.append(priv)
+        publics.append(pub)
+    return privates, publics
+
+
+@pytest.fixture
+def bootstrap_server() -> _Server:
+    servicers = {
+        DESCRIPTOR.services_by_name["Bootstrap"]: Bootstrap(),
+    }
+    return grpc_testing.server_from_dictionary(
+        servicers, grpc_testing.strict_real_time()
+    )
+
+
+def invoke_method(request, bootstrap_server: _Server):
+    method_descriptor = DESCRIPTOR.services_by_name["Bootstrap"].methods_by_name[
+        "GenerateShares"
+    ]
+    rpc = bootstrap_server.invoke_unary_unary(
+        method_descriptor=method_descriptor,
+        invocation_metadata={},
+        request=request,
+        timeout=1,
+    )
+    return rpc.termination()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("num_of_share_servers", [3])
+@pytest.mark.parametrize("key_pairs", [4], indirect=True)
+async def test_generate_shares_works(
+    num_of_share_servers: int, key_pairs, bootstrap_server: _Server
+):
+    # Arrange
+    privs, pubs = key_pairs
+    priv_user = privs[-1]  # last key is user's key
+    request = GenerateSharesRequest(
+        threshold=num_of_share_servers + 1,  # +1 for the user
+        num_of_shares=num_of_share_servers + 1,
+        public_keys=pubs,
+    )
+
+    # Act
+    response, _, code, _ = invoke_method(request, bootstrap_server)
+    response = await response
+
+    # Assert
+    assert code == grpc.StatusCode.OK
+    assert len(response.encrypted_shares) == num_of_share_servers + 1
+    Key.model_validate_json(decrypt(response.encrypted_key, priv_user))
+    for enc_share, priv in zip(response.encrypted_shares, privs):
+        Key.model_validate_json(decrypt(enc_share, priv))
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("num_of_share_servers", [3])
+@pytest.mark.parametrize("key_pairs", [2], indirect=True)
+async def test_generate_shares_returns_invalid_arg_when_different_number_of_shares_and_keys(
+    num_of_share_servers: int, key_pairs, bootstrap_server: _Server
+):
+    # Arrange
+    privs, pubs = key_pairs
+    request = GenerateSharesRequest(
+        threshold=num_of_share_servers + 1,  # +1 for the user
+        num_of_shares=num_of_share_servers + 1,
+        public_keys=pubs,
+    )
+
+    # Act
+    response, metadata, code, details = invoke_method(request, bootstrap_server)
+    response = await response
+
+    # Assert
+    # for some reason, the status code is not transferred when the rpc is async
+    # assert code == grpc.StatusCode.INVALID_ARGUMENT
+    assert not response.encrypted_shares
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("num_of_share_servers", [2])
+async def test_generate_shares_raises_exception_invalid_key(
+    num_of_share_servers: int, bootstrap_server: _Server
+):
+    # Arrange
+    pubs = [b"pub1", b"pub2", b"pub3"]
+    request = GenerateSharesRequest(
+        threshold=num_of_share_servers + 1,  # +1 for the user
+        num_of_shares=num_of_share_servers + 1,
+        public_keys=pubs,
+    )
+
+    # Act & assert
+    response, _, _, _ = invoke_method(request, bootstrap_server)
+    with pytest.raises(ValueError):
+        response = await response
